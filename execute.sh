b751ebf7565c877e1bcd96f0a10bda1db0f7e1ec
Luxxii <dl94@live.de>
2025-06-10 14:50:44 +0200

    Removed file

--- execute.sh
+++
@@ -1,8 +0,0 @@
-nextflow run main_workflow_protein_fasta.nf \
-    -resume \
-    --main_fasta_file ../mus_musculus_UP000000589_2024_02_13.fasta \
-    --main_raw_files_folder ../raws \
-    --main_comet_params ../Goldstandard_search_params.txt\
-    --ctm_num_procs_conversion 8 \
-    --idc_num_parallel_threads_per_search 6 \
-    --qal_limit_num_of_parallel_feature_finders 6
